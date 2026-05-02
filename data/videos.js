6aab6e6e2fceefd59287711f08c358f803a375ed
vidushashashiru-cloud <vidushashashiru@gmail.com>
2026-05-02 07:06:26 +0530

    Fix typo in thumbnails property

--- data/videos.js
+++ data/videos.js
@@ -153,6 +153,7 @@ export const videos = [
     thumbnail: "thumb.jpg",
     popular: true,
     recent: true,
+    thubnails: "file_00000000e78471fdbdde93dd444f7265.png",
   },
 
   // ════════════════════════════════════════════════════
