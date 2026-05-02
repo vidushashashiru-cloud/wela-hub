22cef70a8146519974b59d4ead5b23b1e714034c
vidushashashiru-cloud <vidushashashiru@gmail.com>
2026-05-02 12:15:25 +0530

    Fix video category formatting in videos.js
    Updated video category format in videos.js.

--- data/videos.js
+++ data/videos.js
@@ -27,7 +27,7 @@ export const videos = [
     id: "1",
     title: "පැලවත්තෙ සිතුමි අක්ක ලීක් එක 🥵",
     description: "ඔබේ video description මෙහි.",
-    category: "Sinhala,fuck,blowjob,anal fuck",
+    category: "sinhla new","blowjob","anal fuck",
     views: "4K",
     duration: "17:44",
     date: "2026-05-2",
